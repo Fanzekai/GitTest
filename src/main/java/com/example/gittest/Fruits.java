285416f24174c7d94f795dc4ba992bc9314d3d56
“fzekai” <fzekai@linewell.com>
2024-07-24 00:44:14 +0800

    commit "add apple "

---
+++ src/main/java/com/example/gittest/Fruits.java
@@ -0,0 +1,14 @@
+package com.example.gittest;
+
+/**
+ * @author fzk
+ * @version 1.0
+ * @date 2024/7/24  0:43
+ */
+public class Fruits {
+
+    public static void main(String[] args) {
+
+        System.out.println("");
+    }
+}
