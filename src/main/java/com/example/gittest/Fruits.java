0c75b120ee5b65b3143b476922752487deec50ac
“fzekai” <fzekai@linewell.com>
2024-07-24 01:13:41 +0800

    Revert "commit "add apple ""
    This reverts commit a929a56e

--- src/main/java/com/example/gittest/Fruits.java
+++ src/main/java/com/example/gittest/Fruits.java
@@ -9,8 +9,6 @@ public class Fruits {
 
     public static void main(String[] args) {
 
-        System.out.println("apple");
-
-        System.out.println("banana");
+        System.out.println("");
     }
 }
