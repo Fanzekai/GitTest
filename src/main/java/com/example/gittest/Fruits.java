3d497b0a771ccdb063cfd4370a227289ad6fcc7e
“fzekai” <fzekai@linewell.com>
2024-07-24 00:48:24 +0800

    commit "add apple and banana "

--- src/main/java/com/example/gittest/Fruits.java
+++ src/main/java/com/example/gittest/Fruits.java
@@ -9,6 +9,8 @@ public class Fruits {
 
     public static void main(String[] args) {
 
-        System.out.println("");
+        System.out.println("apple");
+
+        System.out.println("banana");
     }
 }
