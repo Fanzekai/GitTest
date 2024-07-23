eb16837437ed79a45699d2e81329fcc146d4eba2
“fzekai” <fzekai@linewell.com>
2024-07-24 01:25:00 +0800

    commit "合并 master"

--- src/main/java/com/example/gittest/Fruits.java
+++ src/main/java/com/example/gittest/Fruits.java
@@ -9,8 +9,14 @@ public class Fruits {
 
     public static void main(String[] args) {
 
+        System.out.println("apple");
+
+        System.out.println("banana");
+
         System.out.println("Blueberry ");
 
         System.out.println("Date");
+
+        System.out.println("Grape");
     }
 }
